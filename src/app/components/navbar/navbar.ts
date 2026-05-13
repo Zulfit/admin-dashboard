58b3fe6d705fb762f9dd4e582ede9954b71d87fa
Zulfit <106056151+Zulfit@users.noreply.github.com>
2026-05-13 17:06:33 +0800

    push navbar

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HugeiconsIconComponent } from '@hugeicons/angular';
+
+import {
+  StudentIcon,
+  TeacherIcon,
+  Book01Icon
+} from '@hugeicons/core-free-icons';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [HugeiconsIconComponent],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  studentIcon = StudentIcon;
+  teacherIcon = TeacherIcon;
+  bookIcon = Book01Icon;
+}
