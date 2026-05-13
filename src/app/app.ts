4cfc0e1fb1393f42abaf5251fb3790002e76a518
Zulfit <106056151+Zulfit@users.noreply.github.com>
2026-05-13 17:07:14 +0800

    update app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
