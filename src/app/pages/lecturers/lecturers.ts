fb77d818f181451df68a155ceefa0bdd855ce3b1
Zulfit <zulfitrih@gmail.com>
2026-05-22 07:05:51 +0800

    post: create lecturer to the backend

--- src/app/pages/lecturers/lecturers.ts
+++ src/app/pages/lecturers/lecturers.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HugeiconsIconComponent } from '@hugeicons/angular';
+import { HttpClient } from '@angular/common/http';
+import { FormField } from '../../models/form-field';
 
 import { 
   PlusSignIcon,
@@ -16,10 +18,12 @@ import {
   UserGroupIcon
  } from '@hugeicons/core-free-icons';
 import { ButtonCreate } from '../../components/button-create/button-create';
+import { ModalComponent } from '../../components/modal/modal';
+import { DynamicFormComponent } from '../../components/dynamic-form/dynamic-form';
 
 @Component({
   selector: 'app-lecturers',
-  imports: [HugeiconsIconComponent, ButtonCreate],
+  imports: [HugeiconsIconComponent, ButtonCreate, ModalComponent, DynamicFormComponent],
   templateUrl: './lecturers.html',
   styleUrl: './lecturers.css',
 })
@@ -36,4 +40,57 @@ export class Lecturers {
   leftArrowIcon = ArrowLeftIcon;
   rightArrowIcon = ArrowRightIcon;
   userGroupIcon = UserGroupIcon;
+
+  isModalOpen = false;
+
+  private http = inject(HttpClient);
+
+  lecturersFormFields: FormField[] = [
+    { name: 'name', label: 'Lecturer Name', type: 'text', required: true },
+    { name: 'idNumber', label: 'ID Number', type: 'number', required: true },
+    { name: 'email', label: 'Email Address', type: 'email', required: true },
+    { 
+      name: 'course', 
+      label: 'Course', 
+      type: 'select', 
+      required: true, 
+      options: [
+        { label: 'Computer Science', value: 'Computer Science' },
+        { label: 'Business Administration', value: 'Business Administration' },
+        { label: 'Engineering', value: 'Engineering' }
+      ]
+    },
+    { 
+      name: 'status', 
+      label: 'Status', 
+      type: 'select', 
+      required: true, 
+      options: [
+        { label: 'Active', value: 'Active' },
+        { label: 'Inactive', value: 'Inactive' }
+      ]
+    }
+  ];
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  onFormSubmit(data: any) {
+    console.log('Form Submitted!', data);
+
+    this.http.post('http://localhost:3000/lecturers', data).subscribe({
+      next: (response) => {
+        console.log('Lecturer created successfully:', response);
+        this.closeModal();
+      },
+      error: (error) => {
+        console.error('Error creating lecturer:', error);
+      }
+    });
+  }
 }
